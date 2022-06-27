ac539e58873d3fdaae5ae6472b7348072a627aa7
Timaty <101876583+T1maty@users.noreply.github.com>
2022-06-27 13:14:48 +0300

    Add Controller logic

---
+++ Los Pollos Hermanos/ApiModels/RequestModels/LoginUser.cs
@@ -0,0 +1,8 @@
+﻿namespace Los_Pollos_Hermanos.ApiModels.RequestModels
+{
+    public class LoginUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Los Pollos Hermanos/ApiModels/RequestModels/RegisterUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Los_Pollos_Hermanos.ApiModels.RequestModels
+{
+    public class RegisterUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }   
+        public string Password { get; set; }
+    }
+}
